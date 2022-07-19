f1a615308834399881efbd20a3541ebd1d92512f
Sakkurthi Sashank <sakkurthisashank@gmail.com>
2022-07-19 17:27:10 +0530

    remove axios and use default fetch

--- alembic/versions/2683d5f2b63f_create_table_user.py
+++ alembic/versions/2683d5f2b63f_create_table_user.py
@@ -17,14 +17,15 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('user',
+    op.create_table('users',
     sa.Column('id',sa.String,primary_key=True,nullable=False,unique=True),
     sa.Column('email',sa.String,nullable=False,unique=True),
     sa.Column('name',sa.String,nullable=False),
+    sa.Column('hash',sa.String,nullable=False,unique=True),
     sa.Column('password',sa.String,nullable=False),
     sa.Column('created_at',sa.TIMESTAMP(timezone=True),nullable=False, server_default=sa.text('now()')),
     sa.Column('verified',sa.Boolean,server_default='FALSE',nullable=False),
-    sa.Column('active',sa.Boolean,server_default='TRUE',nullable=False)
+    sa.Column('active',sa.Boolean,server_default='TRUE',nullable=False),
     )
 
 def downgrade() -> None:
--- app/router/create_user.py
+++ app/router/create_user.py
@@ -11,7 +11,9 @@ router = APIRouter(prefix="/api/v1/user", tags=['user'])
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 async def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
     new_user = models.User(
-        id=str(uuid4()), email=user.email,name=user.name, password=user.password)
+        id=str(uuid4()), email=user.email,hash = str(uuid4()),
+        name=user.name,password=user.password
+    )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
