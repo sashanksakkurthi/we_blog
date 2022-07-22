49c372d3a2294f09073c39ad112205a83070a368
sakkurthi sashank <sakkurthisashank@gmail.com>
2022-07-22 21:08:28 +0530

    restructure project

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .router import auth_router,create_user,verify_user,post
+from .router import auth_router,create_user,verify_user
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -18,7 +18,6 @@ app.add_middleware(
 app.include_router(create_user.router)
 app.include_router(auth_router.router)
 app.include_router(verify_user.router)
-app.include_router(post.router)
 
 
 @app.get("/")
--- app/models.py
+++ app/models.py
@@ -2,39 +2,6 @@ from sqlalchemy import Column, String, Boolean,ForeignKey
 from .database import Base
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.orm import relationship
-
-
-class Comment(Base):
-    __tablename__ = "comments"
-    id = Column(String,primary_key=True, nullable=False, unique=True)
-    comment = Column(String,nullable=False,unique=False)
-    hash = Column(String,nullable=False, unique=True)
-    post_id =  Column(String, ForeignKey("posts.hash", ondelete="CASCADE"), nullable=False)
-    user_id = Column(String,ForeignKey("users.hash", ondelete="CASCADE"), nullable=False)
-    post = relationship("Post")
-    user = relationship("User")
-
-class Like(Base):
-    __tablename__ = "likes"
-    id = Column(String,primary_key=True, nullable=False, unique=True)
-    hash = Column(String,nullable=False, unique=True)
-    post_id =  Column(String, ForeignKey("posts.hash", ondelete="CASCADE"), nullable=False)
-    user_id = Column(String,ForeignKey("users.hash", ondelete="CASCADE"), nullable=False)
-    post = relationship("Post")
-    user = relationship("User")
-
-class Post(Base):
-    __tablename__ = "posts"
-    id = Column(String,primary_key=True, nullable=False, unique=True)
-    user_id = Column(String, ForeignKey("users.hash", ondelete="CASCADE"), nullable=False)
-    content = Column(String,nullable=True, unique=False)
-    hash = Column(String,nullable=False, unique=True)
-    created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('now()'))
-    published = Column(Boolean, server_default='TRUE', nullable=False)
-    user = relationship("User")
-
 
 class User(Base):
     __tablename__ = "users"
